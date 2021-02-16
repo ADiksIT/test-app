379638e41fe5560983ca8d092592122470adae10
Andrusenko Anton <30493897+ADiksIT@users.noreply.github.com>
2021-02-16 17:17:52 +0200

    delete api key

--- lib/constans.dart
+++ lib/constans.dart
@@ -2,6 +2,5 @@ class ENV {
   static const String API_HOST = 'https://api.unsplash.com';
 
   // paste your api key for unsplash
-  static const String API_KEY =
-      'cf49c08b444ff4cb9e4d126b7e9f7513ba1ee58de7906e4360afc1a33d1bf4c0';
+  static const String API_KEY = '';
 }
